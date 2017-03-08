1a858beee77d9bf5e78de5e8d662d924afb982d5
김수현 <@DESKTOP-1QCUEGH>
2017-03-08 12:33:40 +0900

    sql

--- src/main/webapp/resources/sql/fboard.sql
+++ src/main/webapp/resources/sql/fboard.sql
@@ -2,37 +2,79 @@ drop table fboard;
 drop table fcomment;
 
 create table fboard(
-	f_no		number constraint fb_fno_PK primary key,--�۹�ȣ
-	mem_no		number,--�ۼ��� ȸ�� ��ȣ
-	f_sj		varchar2(100) not null,--�� ����
-	f_ct		clob,--�� ����
-	f_fl		varchar2(1024),--���ε� ���� ���
-	f_rc		number default 0,--��ȸ��
-	f_dt		date default sysdate,--�ۼ��ð�
-	f_lk		number	default 0, --��õ��
-	f_lkno		varchar2(2048)--ȸ����ȣ(��õ��)
+	f_no		number constraint fb_fno_PK primary key,--글번호
+	mem_no		number references member(mem_no),--작성자 회원 번호
+	f_sj		varchar2(100) not null,--글 제목
+	f_ct		clob,--글 내용
+	f_fl		varchar2(1024),--업로드 파일 경로
+	f_rc		number default 0,--조회수
+	f_dt		date default sysdate,--작성시간
+	f_lk		number	default 0, --추천수
+	f_lkno		varchar2(2048) default ',',--회원번호(추천인)
+	fcomm_cnt		number default 0
 );
 
+drop view fboardlist;
+
+create or replace view fboardlist
+as
+select f_no, m.mem_id,f_sj, f_ct, f_fl,f_rc,f_dt,f_lk,f_lkno,fcomm_cnt
+from fboard f, member m 
+where f.mem_no = m.mem_no
+
+/***** fboard 테이블의 시퀀스 생성 *****/
+create sequence fboard_seq 
+increment by 1 start with 1 nocache;
+
+
+create sequence member_seq 
+increment by 1 start with 1 nocache;
+
+insert into mem88(mem_no,mem_id)
+values(mem88_seq.nextval, 'ss');
+
+insert into mem88(mem_no,mem_id)
+values(mem88_seq.nextval, 'ok');
+
+drop table mem88;
 
 
 select * from fboard;
 
-/***** fboard ���̺��� ������ ���� *****/
-create sequence fboard_seq 
-increment by 1 start with 1 nocache;
+
+
+
+select * from FCOMMENT;
+select * from fcommlist;
 
 drop sequence fboard_seq; 
+drop sequence fcomm_seq;
 
 create table fcomment(
-	fcomm_no		number constraint fcomm_fcno_pk primary key,--�ڸ�Ʈ ��ȣ
-	mem_no			number,--�ۼ��� ȸ����ȣ
-	fcomm_ct		varchar2(600) not null,--�ڸ�Ʈ ����
-	fcomm_dt		date,--�ڸ�Ʈ �ۼ���¥
-	fcomm_re_ref	number,--�ڸ�Ʈ�亯������ ��ȣ
-	fcomm_re_lev	number,--�ڸ�Ʈ �亯 ����
-	fcomm_re_seq	number,--�ڸ�Ʈ �亯 ����
-	f_no			number  --������ ��ȣ
+	fcomm_no		number constraint fcomm_fcno_pk primary key,--코멘트 번호
+	mem_no			number,--작성자 회원번호
+	fcomm_ct		varchar2(600) not null,--코멘트 내용
+	fcomm_dt		date,--코멘트 작성날짜
+	fcomm_re_ref	number,--코멘트답변참조글 번호
+	fcomm_re_lev	number,--코멘트 답변 수준
+	fcomm_re_seq	number,--코멘트 답변 순서
+	f_no			number  --참조글 번호
 );
+
+/***** fcomment 테이블의 시퀀스 생성 *****/
+create sequence fcomm_seq 
+increment by 1 start with 1 nocache;
+
+
+create or replace view fcommlist
+as
+select fcomm_no, m.mem_id, fcomm_ct, fcomm_dt, 
+	  fcomm_re_ref, fcomm_re_lev, fcomm_re_seq, f_no
+from fcomment f, member m 
+where f.mem_no = m.mem_no
+
+drop view fcommlist;
+
 
 alter table fboard 
 add constraint fb_mem_no_FK foreign key (mem_no) 
--- src/main/webapp/resources/sql/member.sql
+++ src/main/webapp/resources/sql/member.sql
@@ -1,46 +1,44 @@
 drop table member;
+delete from member;
 
 select * from member;
 
 create table member(   
-	mem_no		number constraint member_memno_pk primary key,--ȸ�� ��ȣ
-    mem_id      varchar2(50) ,
+	mem_no		number constraint member_memno_pk primary key,--회원 번호
+    mem_id      varchar2(50),
     mem_pw      varchar2(50),     
-    mem_nm      varchar2(100),--�̸�
-    mem_sx		number, --����(�� :1, �� :2)
-    mem_bd		date, --�������
-    mem_hp      varchar2(12),--�ڵ�����ȣ
-    mem_tel		varchar2(12),--��ȭ��ȣ
-    mem_zc      number(5),--�����ȣ
-    mem_add1    varchar2(300),--�ּ�
-    mem_add2	varchar2(100),--���ּ�
-    mem_ma      varchar2(100),--�̸���
-    mem_h		number(3,3),--Ű
-    mem_w		number(3,3),--������
-    mem_jd		date default sysdate,--���Գ�¥
-    mem_st		number default 0, --���Ի���(����:0, Ż��:1)
-    mem_dd		date default sysdate,--Ż��¥
-    mem_rs1		varchar2(300),--Ż�����1
-    mem_rs2		varchar2(300)  --Ż�����2
+    mem_nm      varchar2(100),--이름
+    mem_sx		number, --성별(남 :1, 여 :2)
+    mem_bd		date, --생년월일
+    mem_hp      varchar2(100),--핸드폰번호
+    mem_tel		varchar2(100),--전화번호
+    mem_zc      number(20),--우편번호
+    mem_add1    varchar2(300),--주소
+    mem_add2	varchar2(100),--상세주소
+    mem_ma      varchar2(300),--이메일
+    mem_h		number(3),--키
+    mem_w		number(3),--몸무게
+    mem_jd		date default sysdate,--가입날짜
+    mem_wb		varchar2(1024),  --워너비 사진
+    mem_st		number default 0, --가입상태(가입:0, 탈퇴:1)
+    mem_dd		date default sysdate,--탈퇴날짜
+    mem_rs1		varchar2(300) default null,--탈퇴사유1
+    mem_rs2		varchar2(300) default null --탈퇴사유2
 );
 
-/***** member ���̺��� join_code ������ ���� *****/
+
+
+
+/***** member 테이블의 join_code 시퀀스 생성 *****/
 create sequence member_joincode_seq 
 increment by 1 start with 1 nocache;
 
 drop sequence member_joincode_seq; 
 
+
+
 insert into member(mem_no,mem_id,mem_pw, mem_nm,mem_bd,mem_hp,mem_ma,mem_jd)
 values(member_joincode_seq.nextval,'ss','1111','홍길동','19880909','01077778888',
 'ssheln@naver.com',sysdate);
 
-create table zipcode (
-  no number PRIMARY KEY
-  ,ZIPCODE varchar2(7)
-  ,sido varchar2(10) 
-  ,gugun varchar2(20) 
-  ,dong varchar2(50) 
-  ,bunji varchar2(50) 
-);
 
-select * from zipcode;
