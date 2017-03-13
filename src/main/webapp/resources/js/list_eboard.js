87f50df2ae967832fd3855bba928258fe72e5077
angelu1298 <angelu1298@gmail.com>
2017-03-13 16:49:35 +0900

    eboard modify

---
+++ src/main/webapp/resources/js/list_eboard.js
@@ -0,0 +1,28 @@
+  function find_check(){
+            if($.trim($("#srch_txt").val())==""){
+               alert("검색이름을 입력하세요!");
+               $("#srch_txt").val("").focus();
+               return false;
+            }
+         };
+
+    $(document).ready(function(){
+        
+       $("#viewcount").change(function(){
+           var scount=$("#viewcount").val();
+           $.ajax({
+              type:"POST",    
+              url:"eboardList.brn",    
+              data: {"limit" : scount}, 
+              cache: false,
+              headers : {"cache-control" : "no-cache", "pragma" : "no-cache"},
+              success : function(data){               
+                $("body").html(data);
+              },
+             error:function(){
+                 alert("data error");
+            }
+             })//ajax end
+            });//change end
+       
+           });//function end
